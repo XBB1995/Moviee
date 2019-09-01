69dd95d32b543148ff9a549c92cba0e664f8b427
XBB <cs358118276@163.com>
2019-09-01 22:15:26 +0800

    NBA 数据的api太难获取 先用天气api占位

--- src/components/Channels/Nba.vue
+++ src/components/Channels/Nba.vue
@@ -1,23 +1,57 @@
 <template>
     <div>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
-        <p>anba</p>
+        <ul>
+            <li v-for="item of weather" :key="item.day">
+                空气指数 {{item.wea}} {{item.tem}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
-        name: "nba"
+        name: "nba",
+        data() {
+            return {
+                cityId: '',
+                weather: []
+            }
+        },
+        methods: {
+            handleWeather() {
+                let _this = this
+                axios.get('https://www.tianqiapi.com/api/', {
+                    params: {
+                        version: 'v1',
+                        cityid: _this.cityId,
+                        appid: '28636361',
+                        appsecret: 'evU6UF5K'
+                    }
+                })
+                    .then(res => {
+                        let data = res.data
+                        this.weather = data.data
+                    })
+            },
+            handleCityList(city) {
+                axios.get('https://www.easy-mock.com/mock/5d4f6b52bfbd2538192571c7/smzdm/citylist')
+                    .then(res => {
+                        let data = res.data
+                        data.some(val=>{
+                            if(val.cityZh === city||val.cityEn === city){
+                                this.cityId = val.id
+                                return true
+                            }
+                        })
+                    })
+            }
+        },
+        created() {
+            this.handleCityList("beijing")
+            this.handleWeather()
+        }
     }
 </script>
 
